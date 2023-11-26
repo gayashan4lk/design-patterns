c51a3f09d811ede887073f91537954f089768cfd
gayashan4lk <gayashan4lk@gmail.com>
2023-11-26 11:04:53 +0530

    first commit

---
+++ strategy-pattern/strategy-pattern-demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace strategy_pattern_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
