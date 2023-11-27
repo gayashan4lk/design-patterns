57a1fd74aa685363dbeb58ef3f454c143eb4ad09
gayashan4lk <gayashan4lk@gmail.com>
2023-11-28 01:12:17 +0530

    first commit

---
+++ strategy-pattern/simple-strategy-patter-with-DI-factory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace simple_strategy_patter_with_DI_factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
