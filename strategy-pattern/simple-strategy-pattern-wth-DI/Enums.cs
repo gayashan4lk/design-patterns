20a110b02b35a2b85ae4dfc2f1fe7d0c86ad0764
gayashan4lk <gayashan4lk@gmail.com>
2023-11-28 01:09:02 +0530

    replace strings with enums

---
+++ strategy-pattern/simple-strategy-pattern-wth-DI/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_strategy_pattern_wth_DI
+{
+    public enum PaymentServices
+    {
+        PayPal,
+        CreditCard
+    }
+}
--- strategy-pattern/simple-strategy-pattern-wth-DI/Program.cs
+++ strategy-pattern/simple-strategy-pattern-wth-DI/Program.cs
@@ -10,24 +10,24 @@ namespace simple_strategy_pattern_wth_DI
         {
             // Setup dependency injection
             var serviceProvider = new ServiceCollection()
-                .AddTransient<Dictionary<string, IPaymentService>>(provider =>
+                .AddTransient<Dictionary<PaymentServices, IPaymentService>>(provider =>
                 {
-                    var strategies = new Dictionary<string, IPaymentService>
+                    var strategies = new Dictionary<PaymentServices, IPaymentService>
                     {
-                        { "CreditCardPaymentService", new CreditCardPaymentService() },
-                        { "PayPalPaymentService", new PayPalPaymentService() }
+                        { PaymentServices.CreditCard, new CreditCardPaymentService() },
+                        { PaymentServices.PayPal, new PayPalPaymentService() }
                     };
 
                     return strategies;
                 })
                 .BuildServiceProvider();
 
-            var paymentStrategies = serviceProvider.GetRequiredService<Dictionary<string, IPaymentService>>();
+            var paymentStrategies = serviceProvider.GetRequiredService<Dictionary<PaymentServices, IPaymentService>>();
             
 
             // Resolve the required services
-            var creditCardPaymentService = paymentStrategies["CreditCardPaymentService"];
-            var payPalPaymentService = paymentStrategies["PayPalPaymentService"];
+            var creditCardPaymentService = paymentStrategies[PaymentServices.CreditCard];
+            var payPalPaymentService = paymentStrategies[PaymentServices.PayPal];
 
             // Using strategies
             ShoppingCart cart = new ShoppingCart(creditCardPaymentService);
