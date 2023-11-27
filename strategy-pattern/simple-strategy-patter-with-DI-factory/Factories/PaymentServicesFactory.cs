5376024fe49762357f95090f4ade577d6207bbb3
gayashan4lk <gayashan4lk@gmail.com>
2023-11-28 01:32:20 +0530

    update folder structure

---
+++ strategy-pattern/simple-strategy-patter-with-DI-factory/Factories/PaymentServicesFactory.cs
@@ -0,0 +1,30 @@
+﻿using simple_strategy_patter_with_DI_factory.Services;
+
+namespace simple_strategy_patter_with_DI_factory.Factories;
+
+public class PaymentServicesFactory
+{
+	private readonly Dictionary<PaymentServices, Func<IPaymentService>> _paymentServices;
+
+	public PaymentServicesFactory()
+	{
+		_paymentServices = new Dictionary<PaymentServices, Func<IPaymentService>>();
+	}
+
+	public void RegisterService(PaymentServices paymentServiceName, Func<IPaymentService> paymentService)
+	{
+		_paymentServices.Add(paymentServiceName, paymentService);
+	}
+
+	public IPaymentService GetService(PaymentServices paymentServiceName)
+	{
+		// return _paymentServices[paymentServiceName]();
+
+		if (_paymentServices.TryGetValue(paymentServiceName, out var service))
+		{
+			return service();
+		}
+
+		throw new KeyNotFoundException($"Payment service {paymentServiceName} is not registered");
+	}
+}
--- strategy-pattern/simple-strategy-patter-with-DI-factory/Program.cs
+++ strategy-pattern/simple-strategy-patter-with-DI-factory/Program.cs
@@ -1,36 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using simple_strategy_patter_with_DI_factory.Context;
+using simple_strategy_patter_with_DI_factory.Factories;
 using simple_strategy_patter_with_DI_factory.Services;
 
 namespace simple_strategy_patter_with_DI_factory
 {
-    public class PaymentServicesFactory
-    {
-        private readonly Dictionary<PaymentServices, Func<IPaymentService>> _paymentServices;
-
-        public PaymentServicesFactory()
-        {
-            _paymentServices = new Dictionary<PaymentServices, Func<IPaymentService>>();
-        }
-
-        public void RegisterService(PaymentServices paymentServiceName, Func<IPaymentService> paymentService)
-        {
-            _paymentServices.Add(paymentServiceName, paymentService);
-        }
-
-        public IPaymentService GetService(PaymentServices paymentServiceName)
-        {
-            // return _paymentServices[paymentServiceName]();
-
-            if (_paymentServices.TryGetValue(paymentServiceName, out var service))
-            {
-                return service();
-            }
-
-            throw new KeyNotFoundException($"Payment service {paymentServiceName} is not registered");
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
